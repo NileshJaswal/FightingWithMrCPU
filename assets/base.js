8e7d40ae8bc3e14424fa324c328742f8619e29e7
Nilesh Jaswal <arcanenilesh@gmail.com>
2023-10-17 13:08:18 +0530

    Initialized moves and hps in js

---
+++ assets/base.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //Health Intialize
+    let p1_hp = 100;
+    let cpu_hp = 100;
+
+    //Moves inputs
+    let usermove;
+    let cpuI = Math.random() * 4;
+    cpuI = Math.floor(cpuI);
+    let cpu = ["p", "lp", "b", "lb"][cpuI];
+});
